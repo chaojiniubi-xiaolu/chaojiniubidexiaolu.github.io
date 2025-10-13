e163e62896f4962048f18edfe460ed8a9cb22de6
chaojiniubi-xiaolu <133029944+chaojiniubi-xiaolu@users.noreply.github.com>
2025-10-13 13:14:55 +0800

    Add files via upload

--- views.py
+++ views.py
@@ -1,42 +1,23 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.contrib import messages
+
+from main.models import Book, Order, User
+from login.models import Manager
 import hashlib
 
-from .models import Book, Order
-from login.models import User
+def getmd5(s):
+    s += 'orzyadie'
+    md = hashlib.md5()
+    md.update(s.encode('utf-8'))
+    return md.hexdigest()
 
-def booklist(request):
-    if 'state' not in request.COOKIES:
-        return HttpResponseRedirect('/login')
-    if request.COOKIES['state'] == 'manager':
-        return HttpResponseRedirect('/bookManage')
-    if request.COOKIES['state'] != 'user':
-        return HttpResponseRedirect('/login')
-    
-    user = User.objects.get(unum=request.COOKIES['user'])
+def bookManage(request):
+    if 'state' not in request.COOKIES or request.COOKIES['state'] != 'manager' :
+        return HttpResponseRedirect('/loginManager')
     books = Book.objects.all()
-    
     if request.method == 'POST':
-        if 'buy' in request.POST:
-            selected_books = request.POST.getlist('selected_books')
-            if len(selected_books) == 0:
-                return redirect('booklist')
-            book = Book.objects.get(bnum=selected_books[0])
-            if book.overplus == 0:
-                messages.error(request, '图书已售罄，请购买别的图书！')
-                return redirect('booklist')
-            lastnum = 0
-            for x in Order.objects.all():
-                if int(x.onum[1:]) > lastnum:
-                    lastnum = int(x.onum[1:])
-            onum = 'O' + str(lastnum + 1).zfill(5)
-            order = Order(onum=onum, bnum=book, unum=user)
-            order.save()
-            book.overplus -= 1
-            book.save()
-            messages.error(request, '购买成功！')
-        else:
+        if 'search' in request.POST:
             search_type = request.POST.get('search_type')
             if search_type == 'name':
                 book_name = request.POST.get('book_name')
@@ -64,62 +45,143 @@ def booklist(request):
                     books = books.filter(overplus__gte=min_overplus, price__gte=min_price)
                 elif max_price:
                     books = books.filter(overplus__gte=min_overplus, price__lte=max_price)
+        else:
+            selected_books = request.POST.getlist('selected_books')
+            if len(selected_books) == 0:
+                return redirect('bookManage')
+            book = Book.objects.get(bnum=selected_books[0])
+            return HttpResponseRedirect('/bookEdit/' + book.bnum)
+    return render(request, 'bookManage.html', {'books': books})
 
-    
-    return render(request, 'booklist.html', {'books': books, 'username': user.uname})
-
-def orderlist(request):
-    if 'state' not in request.COOKIES or request.COOKIES['state'] != 'user' :
-        return HttpResponseRedirect('/login')
-    user = User.objects.get(unum=request.COOKIES['user'])
-    orders = Order.objects.filter(unum=user.unum)
-    return render(request, 'orderlist.html', {'orders': orders, 'username': user.uname})
-
-def profile(request):
-    if 'state' not in request.COOKIES or request.COOKIES['state'] != 'user' :
-        return HttpResponseRedirect('/login')
-    user = User.objects.get(unum=request.COOKIES['user'])
-    return render(request, 'profile.html', {'usernum': user.unum, 'username': user.uname, 'contact': user.contact, 'address': user.address})
-
-def getmd5(s):
-    s += 'orzyadie'
-    md = hashlib.md5()
-    md.update(s.encode('utf-8'))
-    return md.hexdigest()
-
-def modify(request):
-    if 'state' not in request.COOKIES or request.COOKIES['state'] != 'user' :
-        return HttpResponseRedirect('/login')
-    user = User.objects.get(unum=request.COOKIES['user'])
+def bookEdit(request, bnum):
+    if 'state' not in request.COOKIES or request.COOKIES['state'] != 'manager' :
+        return HttpResponseRedirect('/loginManager')
+    book = Book.objects.get(bnum=bnum)
     if request.method == 'GET':
-        return render(request, 'modify.html', {'username': user.uname})
-    usr = request.POST.get('usr', '')
+        return render(request, 'bookEdit.html', {'book': book})
+    book.btype = request.POST.get('btype', '')
+    book.bname = request.POST.get('name', '')
+    book.price = request.POST.get('price', '')
+    book.overplus = request.POST.get('overplus', '')
+    if int(book.overplus) < 0:
+        messages.error(request, '图书余量不能小于0！')
+        return redirect('/bookEdit/' + bnum)
+    messages.error(request, '修改成功！')
+    book.save()
+    return redirect('bookManage')
+
+def bookAdd(request):
+    if 'state' not in request.COOKIES or request.COOKIES['state'] != 'manager' :
+        return HttpResponseRedirect('/loginManager')
+    if request.method == 'GET':
+        return render(request, 'bookAdd.html')
+    book = Book()
+    book.bnum = request.POST.get('bnum', '')
+    book.btype = request.POST.get('btype', '')
+    book.bname = request.POST.get('name', '')
+    book.price = request.POST.get('price', '')
+    book.overplus = request.POST.get('overplus', '')
+    if int(book.overplus) < 0:
+        messages.error(request, '图书余量不能小于0！')
+        return redirect('/bookAdd')
+    messages.error(request, '创建图书成功！')
+    book.save()
+    return redirect('bookManage')
+
+def bookDelete(request):
+    if 'state' not in request.COOKIES or request.COOKIES['state'] != 'manager' :
+        return HttpResponseRedirect('/loginManager')
+    bnum = request.GET.get('bnum')
+    book = get_object_or_404(Book, bnum=bnum)
+    book.delete()
+    messages.error(request, '删除图书成功！')
+    return redirect('bookManage')
+
+def userOrder(request, num):
+    if 'state' not in request.COOKIES or request.COOKIES['state'] != 'manager' :
+        return HttpResponseRedirect('/loginManager')
+    user = User.objects.get(unum=num)
+    orders = Order.objects.filter(unum=user)
+    return render(request, 'userOrder.html', {'orders': orders})
+
+def userReset(request):
+    if 'state' not in request.COOKIES or request.COOKIES['state'] != 'manager' :
+        return HttpResponseRedirect('/loginManager')
+    unum = request.GET.get('unum')
+    user = User.objects.get(unum=unum)
+    user.passwd = getmd5('123456')
+    user.save()
+    messages.error(request, '密码已重置为123456！')
+    return redirect('userManage')
+
+def userManage(request):
+    if 'state' not in request.COOKIES or request.COOKIES['state'] != 'manager' :
+        return HttpResponseRedirect('/loginManager')
+    users = User.objects.all()
+    if request.method == 'POST':
+        selected_users = request.POST.getlist('selected_users')
+        if len(selected_users) == 0:
+            return redirect('userManage')
+        user = selected_users[0]
+        if 'view' in request.POST:
+            return HttpResponseRedirect('userOrder/' + user)
+        if 'edit' in request.POST:
+            return HttpResponseRedirect('userEdit/' + user)
+    return render(request, 'userManage.html', {'users': users})
+
+def userEdit(request, num):
+    if 'state' not in request.COOKIES or request.COOKIES['state'] != 'manager' :
+        return HttpResponseRedirect('/loginManager')
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        contact = request.POST.get('contact')
+        address = request.POST.get('address')
+        state = request.POST.get('state') == 'on'
+        user = User.objects.get(unum=num)
+        user.uname = name
+        user.contact = contact
+        user.address = address
+        user.state = state
+        user.save()
+        messages.error(request, '保存成功')
+        return redirect('userManage')
+    
+    user = User.objects.get(unum=num)
+    return render(request, 'userEdit.html', {'user': user})
+
+def userVerify(request):
+    if 'state' not in request.COOKIES or request.COOKIES['state'] != 'manager' :
+        return HttpResponseRedirect('/loginManager')
+    users = User.objects.filter(state=False)
+    if request.method == 'GET':
+        return render(request, 'userVerify.html', {'users': users})
+    selected_users = request.POST.getlist('selected_users')
+    for num in selected_users:
+        user = User.objects.get(unum=num)
+        user.state = True
+        user.save()
+    messages.error(request, '已通过选定用户')
+    return redirect('userManage')
+
+def selfModify(request):
+    if 'state' not in request.COOKIES or request.COOKIES['state'] != 'manager' :
+        return HttpResponseRedirect('/loginManager')
+    manager = Manager.objects.get(mnum=request.COOKIES['user'])
+    if request.method == 'GET':
+        return render(request, 'selfModify.html')
     psw = request.POST.get('psw', '')
     npsw = request.POST.get('npsw', '')
     npswc = request.POST.get('npswc', '')
-    contact = request.POST.get('contact', '')
-    address = request.POST.get('address', '')
-    if getmd5(psw) != user.passwd:
+    if getmd5(psw) != manager.passwd:
         messages.error(request, '原密码输入错误！')
-        return redirect('modify')
+        return redirect('selfModify')
     if npsw != npswc:
         messages.error(request, '两次输入的密码不一致！')
-        return redirect('modify')
-    if npsw != '' and len(npsw) < 6:
+        return redirect('selfModify')
+    if len(npsw) < 6:
         messages.error(request, '密码长度至少为6！')
-        return redirect('modify')
-    if usr != '':
-        for x in User.objects.all():
-            if usr == x.uname:
-                messages.error(request, '用户名已存在！')
-                return redirect('modify')
-        user.uname = usr
-    if npsw != '':
-        user.passwd = getmd5(npsw)
-    if contact != '':
-        user.contact = contact
-    if address != '':
-        user.address = address
-    user.save()
+        return redirect('selfModify')
+    manager.passwd = getmd5(npsw)
+    manager.save()
     messages.error(request, '修改成功！')
-    return HttpResponseRedirect('/profile')
+    return redirect('bookManage')
